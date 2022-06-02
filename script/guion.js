a70f5e976d257f2952310288f75688427d0f3b6a
luis <ltrullen@outlook.com>
2022-06-02 14:03:45 +0200

    Primer envio

---
+++ script/guion.js
@@ -0,0 +1,328 @@
+let texto = '<html>
+
+<!--
+ /* Font Definitions */
+ @font-face
+	{font-family:Wingdings;
+	panose-1:5 0 0 0 0 0 0 0 0 0;}
+@font-face
+	{font-family:"Cambria Math";
+	panose-1:2 4 5 3 5 4 6 3 2 4;}
+@font-face
+	{font-family:Calibri;
+	panose-1:2 15 5 2 2 2 4 3 2 4;}
+@font-face
+	{font-family:"Segoe UI Emoji";
+	panose-1:2 11 5 2 4 2 4 2 2 3;}
+@font-face
+	{font-family:"Arial Narrow";
+	panose-1:2 11 6 6 2 2 2 3 2 4;}
+@font-face
+	{font-family:Cambria;
+	panose-1:2 4 5 3 5 4 6 3 2 4;}
+@font-face
+	{font-family:"Century Gothic";
+	panose-1:2 11 5 2 2 2 2 2 2 4;}
+@font-face
+	{font-family:Tahoma;
+	panose-1:2 11 6 4 3 5 4 4 2 4;}
+@font-face
+	{font-family:"Lucida Sans Unicode";
+	panose-1:2 11 6 2 3 5 4 2 2 4;}
+@font-face
+	{font-family:"Swis721 Ex BT";
+	panose-1:0 0 0 0 0 0 0 0 0 0;}
+@font-face
+	{font-family:"Trebuchet MS";
+	panose-1:2 11 6 3 2 2 2 2 2 4;}
+ /* Style Definitions */
+ p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin:0cm;
+	text-align:justify;
+	text-autospace:none;
+	font-size:11.0pt;
+	font-family:"Segoe UI Emoji",sans-serif;}
+h1
+	{mso-style-link:"T�tulo 1 Car";
+	margin-top:24.0pt;
+	margin-right:0cm;
+	margin-bottom:0cm;
+	margin-left:0cm;
+	text-align:left;
+	page-break-after:avoid;
+	background:#215868;
+	text-autospace:none;
+	font-size:26.0pt;
+	font-family:"Cambria",serif;
+	color:white;
+	font-weight:bold;}
+h2
+	{mso-style-link:"T�tulo 2 Car";
+	margin-top:2.0pt;
+	margin-right:0cm;
+	margin-bottom:12.0pt;
+	margin-left:0cm;
+	text-align:left;
+	page-break-after:avoid;
+	background:#31849B;
+	text-autospace:none;
+	font-size:16.0pt;
+	font-family:"Century Gothic",sans-serif;
+	color:white;
+	font-weight:normal;}
+h3
+	{mso-style-link:"T�tulo 3 Car";
+	margin-top:2.0pt;
+	margin-right:0cm;
+	margin-bottom:12.0pt;
+	margin-left:0cm;
+	text-align:left;
+	page-break-after:avoid;
+	background:#DAEEF3;
+	text-autospace:none;
+	border:none;
+	padding:0cm;
+	font-size:11.0pt;
+	font-family:"Century Gothic",sans-serif;
+	color:#243F60;
+	font-weight:bold;}
+h4
+	{mso-style-link:"T�tulo 4 Car";
+	margin-top:2.0pt;
+	margin-right:0cm;
+	margin-bottom:12.0pt;
+	margin-left:0cm;
+	text-align:justify;
+	page-break-after:avoid;
+	background:#F2F2F2;
+	text-autospace:none;
+	border:none;
+	padding:0cm;
+	font-size:11.0pt;
+	font-family:"Century Gothic",sans-serif;
+	color:#365F91;
+	font-weight:bold;}
+h5
+	{mso-style-link:"T�tulo 5 Car";
+	margin-top:12.0pt;
+	margin-right:0cm;
+	margin-bottom:12.0pt;
+	margin-left:0cm;
+	text-align:justify;
+	page-break-after:avoid;
+	punctuation-wrap:simple;
+	text-autospace:none;
+	border:none;
+	padding:0cm;
+	font-size:11.0pt;
+	font-family:"Segoe UI Emoji",sans-serif;
+	font-weight:bold;}
+h6
+	{mso-style-link:"T�tulo 6 Car";
+	margin-top:2.0pt;
+	margin-right:0cm;
+	margin-bottom:0cm;
+	margin-left:0cm;
+	text-align:justify;
+	page-break-after:avoid;
+	text-autospace:none;
+	border:none;
+	padding:0cm;
+	font-size:11.0pt;
+	font-family:"Century Gothic",sans-serif;
+	font-weight:normal;}
+p.MsoHeader, li.MsoHeader, div.MsoHeader
+	{mso-style-link:"Encabezado Car";
+	margin:0cm;
+	text-align:justify;
+	text-autospace:none;
+	font-size:11.0pt;
+	font-family:"Segoe UI Emoji",sans-serif;}
+p.MsoFooter, li.MsoFooter, div.MsoFooter
+	{mso-style-link:"Pie de p�gina Car";
+	margin:0cm;
+	text-align:justify;
+	text-autospace:none;
+	font-size:11.0pt;
+	font-family:"Segoe UI Emoji",sans-serif;}
+p.MsoCaption, li.MsoCaption, div.MsoCaption
+	{margin-top:6.0pt;
+	margin-right:0cm;
+	margin-bottom:6.0pt;
+	margin-left:0cm;
+	text-align:justify;
+	text-autospace:none;
+	font-size:11.0pt;
+	font-family:"Segoe UI Emoji",sans-serif;
+	font-weight:bold;}
+span.MsoPageNumber
+	{font-family:"Times New Roman",serif;}
+p.MsoBodyText, li.MsoBodyText, div.MsoBodyText
+	{mso-style-link:"Texto independiente Car";
+	margin-top:0cm;
+	margin-right:0cm;
+	margin-bottom:6.0pt;
+	margin-left:0cm;
+	text-align:justify;
+	text-autospace:none;
+	font-size:11.0pt;
+	font-family:"Segoe UI Emoji",sans-serif;}
+p.MsoBodyText3, li.MsoBodyText3, div.MsoBodyText3
+	{mso-style-link:"Texto independiente 3 Car";
+	margin:0cm;
+	text-align:justify;
+	punctuation-wrap:simple;
+	text-autospace:none;
+	font-size:11.0pt;
+	font-family:"Segoe UI Emoji",sans-serif;
+	font-weight:bold;}
+a:link, span.MsoHyperlink
+	{font-family:"Times New Roman",serif;
+	color:blue;
+	text-decoration:underline;}
+a:visited, span.MsoHyperlinkFollowed
+	{font-family:"Times New Roman",serif;
+	color:purple;
+	text-decoration:underline;}
+p.MsoPlainText, li.MsoPlainText, div.MsoPlainText
+	{mso-style-link:"Texto sin formato Car";
+	margin:0cm;
+	text-align:justify;
+	text-autospace:none;
+	font-size:11.0pt;
+	font-family:"Courier New";}
+p
+	{margin-right:0cm;
+	margin-left:0cm;
+	text-align:justify;
+	font-size:11.0pt;
+	font-family:"Segoe UI Emoji",sans-serif;}
+p.MsoAcetate, li.MsoAcetate, div.MsoAcetate
+	{mso-style-link:"Texto de globo Car";
+	margin:0cm;
+	text-align:justify;
+	text-autospace:none;
+	font-size:8.0pt;
+	font-family:"Tahoma",sans-serif;}
+p.MsoListParagraph, li.MsoListParagraph, div.MsoListParagraph
+	{margin-top:0cm;
+	margin-right:0cm;
+	margin-bottom:0cm;
+	margin-left:35.4pt;
+	text-align:justify;
+	text-autospace:none;
+	font-size:11.0pt;
+	font-family:"Segoe UI Emoji",sans-serif;}
+span.Ttulo1Car
+	{mso-style-name:"T�tulo 1 Car";
+	mso-style-link:"T�tulo 1";
+	font-family:"Cambria",serif;
+	color:white;
+	background:#215868;
+	font-weight:bold;}
+span.Ttulo3Car
+	{mso-style-name:"T�tulo 3 Car";
+	mso-style-link:"T�tulo 3";
+	font-family:"Century Gothic",sans-serif;
+	color:#243F60;
+	background:#DAEEF3;
+	font-weight:bold;}
+span.Ttulo5Car
+	{mso-style-name:"T�tulo 5 Car";
+	mso-style-link:"T�tulo 5";
+	font-family:"Calibri",sans-serif;
+	font-weight:bold;}
+span.TextosinformatoCar
+	{mso-style-name:"Texto sin formato Car";
+	mso-style-link:"Texto sin formato";
+	font-family:"Courier New";}
+p.Texto9, li.Texto9, div.Texto9
+	{mso-style-name:Texto9;
+	margin:0cm;
+	text-align:justify;
+	text-autospace:none;
+	font-size:9.0pt;
+	font-family:"Courier New";}
+span.EncabezadoCar
+	{mso-style-name:"Encabezado Car";
+	mso-style-link:Encabezado;
+	font-family:"Times New Roman",serif;}
+span.PiedepginaCar
+	{mso-style-name:"Pie de p�gina Car";
+	mso-style-link:"Pie de p�gina";
+	font-family:"Times New Roman",serif;}
+span.hps
+	{mso-style-name:hps;}
+span.A7
+	{mso-style-name:A7;
+	color:black;
+	font-style:italic;}
+p.Pa4, li.Pa4, div.Pa4
+	{mso-style-name:Pa4;
+	margin:0cm;
+	text-align:justify;
+	line-height:12.0pt;
+	text-autospace:none;
+	font-size:11.0pt;
+	font-family:"Arial",sans-serif;}
+p.Pa7, li.Pa7, div.Pa7
+	{mso-style-name:Pa7;
+	margin:0cm;
+	text-align:justify;
+	line-height:12.0pt;
+	text-autospace:none;
+	font-size:11.0pt;
+	font-family:"Arial",sans-serif;}
+span.shorttext
+	{mso-style-name:short_text;}
+span.Textoindependiente3Car
+	{mso-style-name:"Texto independiente 3 Car";
+	mso-style-link:"Texto independiente 3";
+	font-family:"Times New Roman",serif;
+	font-weight:bold;}
+span.TextoindependienteCar
+	{mso-style-name:"Texto independiente Car";
+	mso-style-link:"Texto independiente";
+	font-family:"Times New Roman",serif;}
+span.apple-converted-space
+	{mso-style-name:apple-converted-space;}
+span.fpdescripcion
+	{mso-style-name:fp_descripcion;}
+span.longtext1
+	{mso-style-name:long_text1;}
+span.TextodegloboCar
+	{mso-style-name:"Texto de globo Car";
+	mso-style-link:"Texto de globo";
+	font-family:"Tahoma",sans-serif;}
+span.Ttulo2Car
+	{mso-style-name:"T�tulo 2 Car";
+	mso-style-link:"T�tulo 2";
+	font-family:"Century Gothic",sans-serif;
+	color:white;
+	background:#31849B;}
+span.Ttulo4Car
+	{mso-style-name:"T�tulo 4 Car";
+	mso-style-link:"T�tulo 4";
+	font-family:"Century Gothic",sans-serif;
+	color:#365F91;
+	background:#F2F2F2;
+	font-weight:bold;}
+span.Ttulo6Car
+	{mso-style-name:"T�tulo 6 Car";
+	mso-style-link:"T�tulo 6";
+	font-family:"Century Gothic",sans-serif;}
+.MsoChpDefault
+	{font-size:10.0pt;
+	font-family:"Calibri",sans-serif;}
+ /* Page Definitions */
+ @page WordSection1
+	{size:595.3pt 841.9pt;
+	margin:72.0pt 54.0pt 72.0pt 54.0pt;}
+div.WordSection1
+	{page:WordSection1;}
+ /* List Definitions */
+ ol
+	{margin-bottom:0cm;}
+ul
+	{margin-bottom:0cm;}
+-->
