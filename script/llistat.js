18519f1b73aab7d38a5494a0d112ea257d8eb975
luis <ltrullen@outlook.com>
2022-06-17 13:40:48 +0200

    Viernes

--- script/llistat.js
+++ script/llistat.js
@@ -2,5 +2,6 @@
       ["B1L1", "Galvanizado de la estructura", "1"],
       ["B1L2P1", "Galvanización ZM310", "2"],
       ["B1L2P2", "Galvanización en caliente por inmersión proceso discontinuo", "2"],
-      ["B1L2P3", "Galvanización Sendzimir Z-275", "2"]
+      ["B1L2P3", "Galvanización Sendzimir Z-275", "2"],
+      ["CURVO", "Curvo", "1"],
     ];
