4d072d9c15dbdacb999ea9fd2e33bdadb34e576d
Luis Trullén Gas <ltrullen@gmail.com>
2022-06-30 18:40:20 +0200

    V4

--- script/llistat.js
+++ script/llistat.js
@@ -1,7 +1,7 @@
     let lesDades = [
-      ["B1L1", "Galvanizado de la estructura", "1"],
-      ["B1L2P1", "Galvanización ZM310", "2"],
-      ["B1L2P2", "Galvanización en caliente por inmersión proceso discontinuo", "2"],
-      ["B1L2P3", "Galvanización Sendzimir Z-275", "2"],
-      ["B2L1", "Tipos de puertas", "1"],
+      ["B1L1", "Galvanizado de la estructura", "1","1"],
+      ["B1L2P1", "Galvanización ZM310", "2","1"],
+      ["B1L2P2", "Galvanización en caliente por inmersión proceso discontinuo", "2","0"],
+      ["B1L2P3", "Galvanización Sendzimir Z-275", "2","0"],
+      ["B2L1", "Tipos de puertas", "1","1"],
     ];
---
+++ script/motorv2.js
@@ -0,0 +1,62 @@
+for (i = 0; i < lesDades.length; i++) {
+  let nivell = "";
+  let finnivell = "";
+  let pre = "";
+
+  if (lesDades[i][2] == "2") {
+    nivell = "<ul>";
+    finnivell = "</ul>";
+  } else {
+    nivell = "";
+    finnivell = "";
+  }
+
+  if (lesDades[i][3] == "1") {
+    pre = "checked";
+  }
+
+  document.write(
+    nivell +
+      '<ul><input type="checkbox" numero = ' +
+      [i][0] +
+      " id=" +
+      lesDades[i][0] +
+      " " +
+      pre +
+      '  ><label for="' +
+      lesDades[i][0] +
+      '">' +
+      lesDades[i][1] +
+      "</label><br><hr></ul>" +
+      finnivell
+  );
+}
+function genera() {
+  for (i = 0; i < lesDades.length; i++) {
+    let cadena = "lesDades[i][0]";
+    let valor = eval(cadena);
+    let arxiu = "blocs/" + valor + ".htm";
+    let caixa = document.getElementById(valor);
+    if (caixa.checked) {
+      aborrar = document.getElementById("i" + valor);
+      if (aborrar != null) {
+        document.body.removeChild(aborrar);
+      }
+      fetch(arxiu)
+        .then((response) => {
+          return response.text();
+        })
+        .then((data) => {
+          let noudiv = document.createElement("div");
+          noudiv.id = "i" + valor;
+          document.body.appendChild(noudiv);
+          document.getElementById("i" + valor).innerHTML = data;
+        });
+    } else {
+      aborrar = document.getElementById("i" + valor);
+      if (aborrar != null) {
+        document.body.removeChild(aborrar);
+      }
+    }
+  }
+}
